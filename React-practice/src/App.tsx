b6bb1192359398115f2a8bc246eff4815327c281
DexZed <144045080+DexZed@users.noreply.github.com>
2024-10-23 15:58:06 +0600

    "Updated App component to accept properties, added props to User component with default values, and modified User component to display props"

--- React-practice/src/App.tsx
+++ React-practice/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "./exp.scss";
-function App() {
+function App(properties:object = {idx:1,idx2:2}) { 
   const [count, setCount] = useState(0);
 
   return (
@@ -23,12 +23,21 @@ function App() {
         </button>
       </div>
 
-      <User></User>
+      <User glasses="M" type="S"></User> 
+      {/* Properties needs to be defined in interface */}
+      <User {...properties}></User>
+
     </>
   );
 }
-
-const User: React.FC = () => {
+ // Define the interface for user props
+ interface UserProps {
+  glasses?: string;
+  type?: string; // Optional prop with default value
+  properties?: object;
+}
+ // Default values for properties
+const User: React.FC<UserProps> = ({glasses = "None", type = "N/A",properties={idx :'N/A 234',idx2:'N/A 234'}}) => {
   // TypeScript Declaration
   let age: number = 25;
   let name: string = "John Doe";
@@ -51,10 +60,11 @@ const User: React.FC = () => {
   return (
     <div className='user' style={style}>
       <h2>User Component</h2>
-      <p>This is a user component</p>
+      <p>This is a user component: Gender :{glasses} , Type :  {type}</p>
       <p>Name: {name}</p>
       <p>Age: {age}</p>
       <p>Is Admin: {isAdmin.toString()}</p>
+      <p>properties: {} </p>
     </div>
   );
 };
