e5febba46c7b238fe01f80d039396e32a8159d59
DexZed <144045080+DexZed@users.noreply.github.com>
2024-10-23 15:25:05 +0600

    user component and styles

--- React-practice/src/App.tsx
+++ React-practice/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./exp.scss";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <User></User>
     </>
-  )
+  );
 }
 
-export default App
+const User: React.FC = () => {
+  // TypeScript Declaration
+  let age: number = 25;
+  let name: string = "John Doe";
+  let isAdmin: boolean = true;
+  // Inline Styles
+  var style: {
+    backgroundColor: string;
+    color: string;
+    padding: string;
+    borderRadius: string;
+    border: string
+  } = {
+    backgroundColor: isAdmin ? "green" : "red", //sets color base on isadmin bool value
+    color: "Blue",
+    padding: "10px",
+    borderRadius: "5px",
+    border:"thin solid lightblue"
+  };
+  // Dynamic Class Name + SCSS Class 
+  return (
+    <div className='user' style={style}>
+      <h2>User Component</h2>
+      <p>This is a user component</p>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Is Admin: {isAdmin.toString()}</p>
+    </div>
+  );
+};
+export default App;
